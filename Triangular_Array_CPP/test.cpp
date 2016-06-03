d7508842a2eace75e4574feef88cb56ba9033530
Zackary Murphy <zmurphy1125@gmail.com>
2016-06-03 12:08:57 -0500

    Fixed initialization issues
    Fixed initialization issues, performed more thorough testing. Also added
    print function for ease of use. Still need to add iterator.

--- Triangular_Array_CPP/test.cpp
+++ Triangular_Array_CPP/test.cpp
@@ -1,5 +1,5 @@
+#include <string>
 #include "triangular_array.h"
-
 void init_test();
 
 int main() {
@@ -9,6 +9,59 @@ int main() {
 }
 
 void init_test() {
-    Triangular_Array<int> i_test_array();
+    //Test default constructor
+    Triangular_Array<int> i_test_array;
+    //Test non-default constructor for floats
     Triangular_Array<float> f_test_array(20);
+    //Test non-default constructor for doubles
+    Triangular_Array<double> d_test_array(100);
+    //Test setting
+    for (unsigned int i = 0; i < i_test_array.get_dimension(); i++) {
+        for (unsigned int j = 0; j <= i; j++) {
+            i_test_array.set_element(i, j, 99);
+        }
+    }
+    for (unsigned int i = 0; i < f_test_array.get_dimension(); i++) {
+        for (unsigned int j = 0; j <= i; j++) {
+            f_test_array.set_element(i, j, 137.7f);
+        }
+    }
+    for (unsigned int i = 0; i < d_test_array.get_dimension(); i++) {
+        for (unsigned int j = 0; j <= i; j++) {
+            d_test_array.set_element(i, j, 99.99);
+        }
+    }
+    i_test_array.print();
+    //f_test_array.print();
+    //d_test_array.print();
+    i_test_array.resize(15);
+    for (unsigned int i = 0; i < i_test_array.get_dimension(); i++) {
+        for (unsigned int j = 0; j <= i; j++) {
+            i_test_array.set_element(i, j, i+j);
+        }
+    }
+    i_test_array.print();
+    i_test_array.resize(8);
+    i_test_array.print();
+
+    try {
+        i_test_array.get_element(9, 0);
+    }
+    catch (std::out_of_range& e) {
+        std::cout<<e.what()<<std::endl;
+    }
+
+    try {
+        i_test_array.get_element(0, 9);
+    }
+    catch (std::out_of_range& e) {
+        std::cout<<e.what()<<std::endl;
+    }
+
+    try {
+        i_test_array.set_element(0, 9, 9);
+    }
+    catch (std::out_of_range& e) {
+        std::cout<<e.what()<<std::endl;
+    }
 }
--- Triangular_Array_CPP/triangular_array.h
+++ Triangular_Array_CPP/triangular_array.h
@@ -1,8 +1,12 @@
 #ifndef TRIANGULAR_ARRAY_H_INCLUDED
 #define TRIANGULAR_ARRAY_H_INCLUDED
-
+#include <iostream>
 #include <vector>
 #include <stdexcept>
+
+/*
+TODO: ADD ITERATOR
+*/
 
 template <typename data_type>
 class Triangular_Array {
@@ -10,10 +14,10 @@ class Triangular_Array {
     protected:
         unsigned int dimension;
         std::vector<std::vector<data_type>> rows;
-        void range_check(unsigned int row, unsigned int col);
-        void column_range_check(unsigned int row, unsigned int col);
+        void range_check(unsigned int row, unsigned int col) const;
+        void column_range_check(unsigned int row, unsigned int col) const;
     public:
-        Triangular_Array(unsigned int desired_dimension = 8);
+        explicit Triangular_Array(unsigned int desired_dimension = 8);
 
         data_type get_element(unsigned int row = 0, unsigned int col = 0) const;
 
@@ -22,6 +26,8 @@ class Triangular_Array {
         void resize(unsigned int new_size);
 
         unsigned int get_dimension();
+
+        void print() const;
 };
 
 template <typename data_type>
@@ -29,19 +35,19 @@ Triangular_Array<data_type>::Triangular_Array(unsigned int desired_dimension) :
     dimension(desired_dimension), rows(desired_dimension)
 {
     for (unsigned int i = 0; i < desired_dimension; i++) {
-        rows[i] = new std::vector<data_type>(i+1);
+        rows[i].resize(i+1);
     }
 }
 
 template <typename data_type>
-void Triangular_Array<data_type>::range_check(unsigned int row, unsigned int col) {
+void Triangular_Array<data_type>::range_check(unsigned int row, unsigned int col) const {
     if (row >= dimension || col > row) {
         throw std::out_of_range("Triangular Array Out of Range Error: Desired element is out of range\n");
     }
 }
 
 template <typename data_type>
-void Triangular_Array<data_type>::column_range_check(unsigned int row, unsigned int col) {
+void Triangular_Array<data_type>::column_range_check(unsigned int row, unsigned int col) const {
     if (col > row) {
         throw std::out_of_range("Triangular Array Out of Range Error: Column value can not be greater than Row value\n");
     }
@@ -67,7 +73,7 @@ void Triangular_Array<data_type>::resize(unsigned int new_size) {
     rows.resize(new_size);
     if (new_size > dimension) {
         for (unsigned int i = dimension; i < new_size; i++) {
-            rows[i] = new std::vector<data_type>(i+1);
+            rows[i].resize(i+1);
         }
     }
     dimension = new_size;
@@ -76,6 +82,17 @@ void Triangular_Array<data_type>::resize(unsigned int new_size) {
 template <typename data_type>
 unsigned int Triangular_Array<data_type>::get_dimension() {
     return dimension;
+}
+
+template <typename data_type>
+void Triangular_Array<data_type>::print() const {
+    for (unsigned int i = 0; i < dimension; i++) {
+        for (unsigned int j = 0; j <= i; j++) {
+            std::cout<<rows[i][j]<<" ";
+        }
+        std::cout<<std::endl;
+    }
+    std::cout<<std::endl;
 }
 
 #endif // TRIANGULAR_ARRAY_H_INCLUDED
